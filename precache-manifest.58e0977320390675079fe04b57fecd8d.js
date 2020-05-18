3de698894b946e02a6f02e9b769e87f2abe11657
ANDRES JAHR <asjahr@uc.cl>
2020-05-17 20:16:30 -0400

    Updates

---
+++ precache-manifest.58e0977320390675079fe04b57fecd8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc17f1992f651871119c8290e2b73189",
+    "url": "/ajahr95/stock-market-real-time/index.html"
+  },
+  {
+    "revision": "6633d024f3322af852bb",
+    "url": "/ajahr95/stock-market-real-time/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e799bacc1daf78d41871",
+    "url": "/ajahr95/stock-market-real-time/static/css/main.28f6cf86.chunk.css"
+  },
+  {
+    "revision": "6633d024f3322af852bb",
+    "url": "/ajahr95/stock-market-real-time/static/js/2.ba902d94.chunk.js"
+  },
+  {
+    "revision": "c04130d8b6692f26b622ca7bd7d5f6b5",
+    "url": "/ajahr95/stock-market-real-time/static/js/2.ba902d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e799bacc1daf78d41871",
+    "url": "/ajahr95/stock-market-real-time/static/js/main.9a3e4e14.chunk.js"
+  },
+  {
+    "revision": "8a8841c983bc5fd43750",
+    "url": "/ajahr95/stock-market-real-time/static/js/runtime-main.a9a1659a.js"
+  }
+]);
